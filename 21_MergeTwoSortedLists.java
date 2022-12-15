edc4c26eed57a6522f30012bc19eedf41b5d55f8
Lourdes-Santhosh <75793822+Lourdes-Santhosh@users.noreply.github.com>
2022-12-15 19:19:34 +0530

    Update 21_MergeTwoSortedLists.java

--- 21_MergeTwoSortedLists.java
+++ 21_MergeTwoSortedLists.java
@@ -25,24 +25,25 @@ Output: [1,1,2,3,4,4]
  * }
  */
 class Solution {
-    
-
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
-        ListNode cur = prehead;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
+    public ListNode mergeTwoLists(ListNode t1, ListNode t2) {
+        ListNode result = new ListNode(9696969);
+        ListNode temp = result;
+        while(t1 != null && t2 != null) {
+            if(t1.val <= t2.val) {
+                temp.next = t1;
+                t1 = t1.next;
             } else {
-                cur.next = l2;
-                l2 = l2.next;
+                temp.next = t2;
+                t2 = t2.next;
             }
-            cur = cur.next;
+            temp = temp.next;
         }
-
-        cur.next = l1 == null ? l2 : l1;
-        return prehead.next;
+        if(t1==null) {
+            temp.next=t2;
+        } else {
+            temp.next=t1;
+        }
+        return result.next;
+        
     }
 }
